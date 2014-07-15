bcc7954e0ee787a1aefb09ad8763059e83c86c0e
andrew <andrew@plato>
2014-07-15 10:40:59 +0100

    Added a theme-settings.php file and added font style options to it

--- all/themes/grayscale/theme-settings.php
+++ all/themes/grayscale/theme-settings.php
@@ -18,25 +18,25 @@ function grayscale_form_system_theme_settings_alter(&$form, &$form_state) {
     $form['styles']['font']['font_family'] = array(
         '#type' => 'select',
         '#title' => t('Font family'),
-        '#default_value' => theme_get_settings('font_family'),
+        '#default_value' => theme_get_setting('font_family'),
         '#options' => array(
-            'ff-sss' => t('Helvetica Nueue, Trebuchet MS, Arial, Nimbus Sans L,'
-                    . 'sans-serif'),
+            'ff-sss' => t('Helvetica Nueue, Trebuchet MS, Arial, Nimbus Sans L, '
+                    . 'FreeSans, sans-serif'),
             'ff-ssl' => t('Verdana, Geneva, Arial, Helvetica, sans-serif'),
             'ff-a' => t('Arial, Helvetica, sans-serif'),
             'ff-ss' => t('Garamond, Perpetua, Nimbus Roman No9 L, Times New Roman, serif'),
-            'ff-sl' => t('Baskerville, Georgia, Palatino, Palatino Linotype,'
-                    . 'Book Antiqua, URW, Palladio L, serif'),
+            'ff-sl' => t('Baskerville, Georgia, Palatino, Palatino Linotype, '
+                    . 'Book Antiqua, URW Palladio L, serif'),
             'ff-m' => t('Myriad Pro, Myriad, Arial, Helvetica, sans-serif'),
-            'ff-l' => t('Lucida Sans, Lucida Grande, Lucida Sans Unicode, Verdana'
+            'ff-l' => t('Lucida Sans, Lucida Grande, Lucida Sans Unicode, Verdana, '
                     . 'Geneva, sans-serif'),
         ),
     );
     $form['styles']['font']['font_size'] = array(
         '#type' => 'select',
         '#title' => t('Font size'),
-        '#default_value' => theme_get_settings('font_size'),
-        '#description' => t('Font sizes are always set in relative units - the'
+        '#default_value' => theme_get_setting('font_size'),
+        '#description' => t('Font sizes are always set in relative units - the '
                 . 'sizes shown are the pixel value equivalent.'),
         '#options' => array(
             'fs-10' => t('10px'),
